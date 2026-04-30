5e452d21d3c8280ac00a8f86bd756c3a553ca586
Sidown <lefrere.corentin@gmail.com>
2026-04-30 17:04:31 +0200

    config.json + started parsing

---
+++ parser.py
@@ -0,0 +1,84 @@
+import json
+import sys
+from pydantic import BaseModel, ValidationError
+
+class Config(BaseModel):
+    highscore_filename: str
+    lives: int
+    level_max_time: int
+    seed: int
+    points_per_pacgum: int
+    points_per_super_pacgum: int
+    points_per_ghost: int
+    levels: list[dict[str, int]]
+
+
+def load_config(path: str) -> dict:
+    try:
+        with open(path, 'r') as f:
+            lines = [line for line in f if not line.strip().startswith('#')]
+            config = "".join(lines)
+
+            if not config:
+                raise ValueError("Config file empty")
+            
+            return json.loads(config)
+        
+    except json.JSONDecodeError:
+        print("Error: JSON not valid")
+        sys.exit()
+    
+    except FileNotFoundError:
+        print(f"File {path} not found")
+        sys.exit("")
+
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit()
+
+
+def config_check(config: Config) -> bool:
+    errors = []
+    try:
+        open(config.highscore_filename, 'r')
+    except FileNotFoundError:
+        errors.append(f"File not found at {config.highscore_filename}")
+    if config.lives <= 0:
+        errors.append(f"Lives must be potive int (currently {config.lives})")
+    if config.level_max_time <= 0:
+        errors.append(f"Level max time must be positive int (currently {config.level_max_time})")
+    if config.points_per_pacgum <= 0:
+        errors.append(f"Points per pacgum must be positive int (currently {config.points_per_pacgum})")
+    if config.points_per_super_pacgum <= 0:
+        errors.append(f"Point per super pacgum must be positive int (currently {config.points_per_super_pacgum})")
+    if config.points_per_ghost <= 0:
+        errors.append(f"Points per ghost must be postivie int (currently {config.points_per_ghost})")
+    if len(config.levels) < 10:
+        errors.append(f"The game must have at least 10 levels (currently {len(config.levels)})")
+    for level in config.levels:
+        if len(level) != 2:
+            errors.append("A level must only contain a width and height")
+        if level['width'] < 0:
+            errors.append("The width of a level must be postive int")
+        if level['height'] < 0:
+            errors.append("The height of a level must be positive int")
+    if errors: 
+        for error in errors:
+            print(f"Error: {error}")
+        return False
+    return True
+    
+
+
+def parser(path: str) -> Config:
+    config = Config(**load_config(path))
+    if config_check(config):
+        return config
+    else:
+        sys.exit()
+
+
+
+
+test = parser("./config.json")
+print(test)
