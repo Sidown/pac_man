f0d70d6abf437f1b03dd2de263dbec81cd167fe2
Clement <clement@fedora.home>
2026-05-06 15:24:35 +0200

    first test of using the mazegenerator class

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import parser
 from visualizer import Visualizer
+from mazegenerator.mazegenerator import MazeGenerator
+
+
 
 def main():
     print("Hello from pac-man!")
@@ -11,6 +14,13 @@ def main():
     # Visualisation
     gui = Visualizer()
     gui.run()
+    mazegenerator = MazeGenerator(
+        size = (15, 15),
+        entry_cell = (0, 0),
+        exit_cell = (-1, -1),
+        seed = 42,
+    )
+    mazegenerator.generate()
 
 
 if __name__ == "__main__":
